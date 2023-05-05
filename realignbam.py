a84189e2a4dddcca55e5c5a22d946c21ed9edc7f
sclokie <s.clokie@nhs.net>
2023-05-05 11:17:15 +0100

    Accommodate missing NMs. Chunk tmp data

--- realignbam.py
+++ realignbam.py
@@ -10,6 +10,7 @@ def main(args):
     output_bam_file = args.output
     nm_threshold = args.nm
     temp_dir = args.tempdir
+    max_reads = args.max_reads
 
     # Open input BAM
     if input_bam_file == '-':
@@ -17,16 +18,30 @@ def main(args):
     else:
         input_bam = pysam.AlignmentFile(input_bam_file, "rb")
 
-    filtered_reads = filter_sam(input_bam, nm_threshold)
-
     # Create a temporary file for storing unsorted output BAM
     with tempfile.NamedTemporaryFile(mode='wb', delete=False, dir=temp_dir) as tmp_file:
         tmp_output_bam_file = tmp_file.name
 
     # Write output BAM
     with pysam.AlignmentFile(tmp_output_bam_file, "wb", header=input_bam.header) as output_bam:
-        for read in filtered_reads:
-            output_bam.write(read)
+        read_counter = 0
+        read_buffer = []
+        for read in input_bam.fetch():
+            read_buffer.append(read)
+            read_counter += 1
+
+            if read_counter >= max_reads:
+                filtered_reads = filter_sam(read_buffer, nm_threshold)
+                for filtered_read in filtered_reads:
+                    output_bam.write(filtered_read)
+                read_buffer = []
+                read_counter = 0
+
+        # Process the remaining reads in the buffer
+        if read_buffer:
+            filtered_reads = filter_sam(read_buffer, nm_threshold)
+            for filtered_read in filtered_reads:
+                output_bam.write(filtered_read)
 
     input_bam.close()
 
@@ -44,6 +59,7 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--output", required=True, help="Output BAM file, use '-' for stdout")
     parser.add_argument("-n", "--nm", type=int, default=1, help="NM threshold (default: 1)")
     parser.add_argument("-t", "--tempdir", default=None, help="Temporary directory for storing intermediate files (default: system temporary directory)")
+    parser.add_argument("-m", "--max_reads", type=int, default=100000, help="Maximum number of reads to process per chunk (default: 100000)")
 
     args = parser.parse_args()
     main(args)
--- utilities.py
+++ utilities.py
@@ -1,37 +1,33 @@
 import sys
 import re
 from collections import deque
-import pysam
-from typing import List
 
 def passes_conditions(column_5, column_6, nm, nm_threshold):
-    if column_6 is None:
-        return False
-
     h_or_s_count = column_6.count('H') + column_6.count('S')
     m_count = column_6.count('M')
+    numbers = [int(num) for num in re.findall(r'\d+', column_6)]
 
-    if nm is None:
-        return column_5 > 50 and h_or_s_count == 1 and m_count == 1
-    else:
-        return column_5 > 50 and h_or_s_count == 1 and m_count == 1 and nm <= nm_threshold
+    return column_5 > 50 and h_or_s_count == 1 and m_count == 1 and all(num > 5 for num in numbers) and nm <= nm_threshold
 
-
-
-def filter_sam(bam: pysam.AlignmentFile, nm_threshold: int = 2) -> List[pysam.AlignedSegment]:
+def filter_sam(reads, nm_threshold=1):
     stored_data = {}
     keys_queue = deque(maxlen=1000)
     filtered_reads = []
 
-    for read in bam.fetch():
-        line = read.to_string()
-        columns = line.split('\t')
+    for read in reads:
+        sam_line = read.to_string()
+        columns = sam_line.split('\t')
 
         if len(columns) >= 6:
             key = columns[0]
-            column_5 = read.mapping_quality
-            column_6 = read.cigarstring
-            nm = read.get_tag('NM') if read.has_tag('NM') else None
+            column_5 = int(columns[4])
+            column_6 = columns[5]
+
+            # Check if the NM tag is present before attempting to access it
+            if read.has_tag('NM'):
+                nm = read.get_tag('NM')
+            else:
+                continue
 
             if key not in stored_data:
                 stored_data[key] = []
@@ -47,9 +43,8 @@ def filter_sam(bam: pysam.AlignmentFile, nm_threshold: int = 2) -> List[pysam.Al
             if passes_conditions(column_5, column_6, nm, nm_threshold):
                 matching_reads = stored_data[key]
                 for matching_read in matching_reads:
-                    if matching_read.cigarstring and matching_read.cigarstring.count('M') == 1:
+                    # Add a check for one M only
+                    if matching_read.cigarstring.count('M') == 1:
                         filtered_reads.append(matching_read)
 
     return filtered_reads
-
-
